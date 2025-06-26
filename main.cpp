a059aa150c6e8ae7b17e36cb1a42701d1cd15e78
swarajholmukhe <swarajholmukhe2005@gmail.com>
2025-06-26 12:07:40 +0530

    Uploaded full DSA Practice Tracker project

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    int count;
+    cout << "Enter number of problems solved today: ";
+    cin >> count;
+
+    ofstream file("leaderboard.txt", ios::app);
+    if (file.is_open()) {
+        file << "User: Swaraj, Problems Solved: " << count << "\n";
+        cout << "Progress saved successfully!\n";
+    } else {
+        cout << "Error opening leaderboard file.\n";
+    }
+
+    file.close();
+    return 0;
+}
  
